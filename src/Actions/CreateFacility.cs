185712d9c2b80f5d83f0d5485ade6042254d1fb7
jwgilliam <ggsmoustache@gmail.com>
2020-03-13 15:26:51 -0500

    working on chicken houses

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -3,26 +3,30 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class CreateFacility {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Grazing field");
-            Console.WriteLine ("2. Plowed field");
+namespace Trestlebridge.Actions
+{
+  public class CreateFacility
+  {
+    public static void CollectInput(Farm farm)
+    {
+      Console.WriteLine("1. Grazing field");
+      Console.WriteLine("2. Plowed field");
+      Console.WriteLine("3. Chicken House");
 
-            Console.WriteLine ();
-            Console.WriteLine ("Choose what you want to create");
+      Console.WriteLine();
+      Console.WriteLine("Choose what you want to create");
 
-            Console.Write ("> ");
-            string input = Console.ReadLine ();
+      Console.Write("> ");
+      string input = Console.ReadLine();
 
-            switch (Int32.Parse(input))
-            {
-                case 1:
-                    farm.AddGrazingField(new GrazingField());
-                    break;
-                default:
-                    break;
-            }
-        }
+      switch (Int32.Parse(input))
+      {
+        case 1:
+          farm.AddGrazingField(new GrazingField());
+          break;
+        default:
+          break;
+      }
     }
+  }
 }
