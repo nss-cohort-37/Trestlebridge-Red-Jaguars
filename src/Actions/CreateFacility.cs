472b54df4106405861143174e3af7b412b2c66b1
Coffey May <jcoffeymay6@gmail.com>
2020-03-13 11:44:42 -0500

    Create grazing field

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
@@ -8,6 +9,9 @@ namespace Trestlebridge.Actions {
         public static void CollectInput (Farm farm) {
             Console.WriteLine ("1. Grazing field");
             Console.WriteLine ("2. Plowed field");
+            Console.WriteLine ("3. Natural Field");
+            Console.WriteLine ("4. Chicken House");
+            Console.WriteLine ("5. Duck House");
 
             Console.WriteLine ();
             Console.WriteLine ("Choose what you want to create");
@@ -15,11 +19,32 @@ namespace Trestlebridge.Actions {
             Console.Write ("> ");
             string input = Console.ReadLine ();
 
-            switch (Int32.Parse(input))
-            {
+            switch (Int32.Parse (input)) {
                 case 1:
-                    farm.AddGrazingField(new GrazingField());
+                    farm.AddGrazingField (new GrazingField ());
+                    Console.WriteLine ("Congratulations, new grazing field created!!!");
+                    Thread.Sleep (TimeSpan.FromSeconds (2.5));
                     break;
+                    // case 2:
+                    //     farm.AddPlowedField (new PlowedField ());
+                    //     Console.WriteLine ("Congratulations, new plowed field created!!!");
+                    //     Thread.Sleep (TimeSpan.FromSeconds (2.5));
+                    //     break;
+                    // case 3:
+                    //     farm.AddNaturalField (new NaturalField ());
+                    //     Console.WriteLine ("Congratulations, new natural field created!!!");
+                    //     Thread.Sleep (TimeSpan.FromSeconds (2.5));
+                    //     break;
+                    // case 4:
+                    //     farm.AddChickenHouse (new ChickenHouse ());
+                    //     Console.WriteLine ("Congratulations, new chicken house created!!!");
+                    //     Thread.Sleep (TimeSpan.FromSeconds (2.5));
+                    //     break;
+                    // case 5:
+                    //     farm.AddDuckHouse (new DuckHouse ());
+                    //     Console.WriteLine ("Congratulations, new duck house created!!!");
+                    //     Thread.Sleep (TimeSpan.FromSeconds (2.5));
+                    //     break;
                 default:
                     break;
             }
--- src/Models/Farm.cs
+++ src/Models/Farm.cs
@@ -4,41 +4,37 @@ using System.Text;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Models
-{
-    public class Farm
-    {
-        public List<GrazingField> GrazingFields { get; } = new List<GrazingField>();
+namespace Trestlebridge.Models {
+    public class Farm {
+        public List<GrazingField> GrazingFields { get; } = new List<GrazingField> ();
 
         /*
             This method must specify the correct product interface of the
             resource being purchased.
          */
-        public void PurchaseResource<T> (IResource resource, int index)
-        {
-            Console.WriteLine(typeof(T).ToString());
-            switch (typeof(T).ToString())
-            {
+        public void PurchaseResource<T> (IResource resource, int index) {
+            Console.WriteLine (typeof (T).ToString ());
+            switch (typeof (T).ToString ()) {
                 case "Cow":
-                    GrazingFields[index].AddResource((IGrazing)resource);
+                    GrazingFields[index].AddResource ((IGrazing) resource);
                     break;
                 default:
                     break;
             }
         }
 
-        public void AddGrazingField (GrazingField field)
-        {
-            GrazingFields.Add(field);
+        public void AddGrazingField (GrazingField field) {
+
+            GrazingFields.Add (field);
+
         }
 
-        public override string ToString()
-        {
-            StringBuilder report = new StringBuilder();
+        public override string ToString () {
+            StringBuilder report = new StringBuilder ();
 
-            GrazingFields.ForEach(gf => report.Append(gf));
+            GrazingFields.ForEach (gf => report.Append (gf));
 
-            return report.ToString();
+            return report.ToString ();
         }
     }
 }
