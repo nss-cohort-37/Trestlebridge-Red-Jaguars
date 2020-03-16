0cfa839f20ce598a81b2d41c400828f8d813cac5
tylerdav <tdavis1433@att.net>
2020-03-16 09:54:39 -0500

    added duckHouse

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -4,9 +4,12 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseStock {
-        public static void CollectInput (Farm farm) {
+namespace Trestlebridge.Actions
+{
+    public class PurchaseStock
+    {
+        public static void CollectInput (Farm farm)
+        {
             Console.WriteLine ("1. Chicken");
             Console.WriteLine ("2. Cow");
             Console.WriteLine ("3. Duck");
@@ -17,32 +20,32 @@ namespace Trestlebridge.Actions {
 
             Console.WriteLine ();
             Console.WriteLine ("What are you buying today?");
-        
+
             Console.Write ("> ");
             string choice = Console.ReadLine ();
 
-            switch (Int32.Parse(choice))
+            switch (Int32.Parse (choice))
             {
                 case 1:
-                    ChooseGrazingField.CollectInput(farm, new Chicken());
+                    ChooseGrazingField.CollectInput (farm, new Chicken ());
                     break;
                 case 2:
-                    ChooseGrazingField.CollectInput(farm, new Cow());
+                    ChooseGrazingField.CollectInput (farm, new Cow ());
                     break;
                 case 3:
-                    ChooseGrazingField.CollectInput(farm, new Duck());
+                    ChooseDuckHouse.CollectInput (farm, new Duck ());
                     break;
                 case 4:
-                    ChooseGrazingField.CollectInput(farm, new Goat());
+                    ChooseGrazingField.CollectInput (farm, new Goat ());
                     break;
                 case 5:
-                    ChooseGrazingField.CollectInput(farm, new Ostrich());
+                    ChooseGrazingField.CollectInput (farm, new Ostrich ());
                     break;
                 case 6:
-                    ChooseGrazingField.CollectInput(farm, new Pig());
+                    ChooseGrazingField.CollectInput (farm, new Pig ());
                     break;
                 case 7:
-                    ChooseGrazingField.CollectInput(farm, new Sheep());
+                    ChooseGrazingField.CollectInput (farm, new Sheep ());
                     break;
                 default:
                     break;
--- src/Models/Animals/Duck.cs
+++ src/Models/Animals/Duck.cs
@@ -2,17 +2,20 @@ using System;
 using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 
-namespace Trestlebridge.Models.Animals {
-    public class Duck : IResource, IEggProducing , IFeatherProducing, IGrazing{
+namespace Trestlebridge.Models.Animals
+{
+    public class Duck : IResource, IEggProducing, IFeatherProducing, IDucks
+    {
 
-        private Guid _id = Guid.NewGuid();
+        private Guid _id = Guid.NewGuid ();
         private double _eggProduced = 6;
         private double _featherProduced = .75;
 
-
-        private string _shortId {
-            get {
-                return this._id.ToString().Substring(this._id.ToString().Length - 6);
+        private string _shortId
+        {
+            get
+            {
+                return this._id.ToString ().Substring (this._id.ToString ().Length - 6);
             }
         }
 
@@ -20,18 +23,22 @@ namespace Trestlebridge.Models.Animals {
         public string Type { get; } = "Duck";
 
         // Methods
-        public void Graze () {
-            Console.WriteLine($"Duck {this._shortId} just ate {this.GrassPerDay}kg of grass");
+        public void Graze ()
+        {
+            Console.WriteLine ($"Duck {this._shortId} just ate {this.GrassPerDay}kg of grass");
         }
 
-        public double GatherEggs () {
+        public double GatherEggs ()
+        {
             return _eggProduced;
         }
-        public double GatherFeathers () {
+        public double GatherFeathers ()
+        {
             return _featherProduced;
         }
 
-        public override string ToString () {
+        public override string ToString ()
+        {
             return $"Duck {this._shortId}. Quack!";
         }
     }
