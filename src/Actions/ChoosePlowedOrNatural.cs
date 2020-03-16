db004dcc3fbebe2dcec2f569b576c6cddbb42a03
jwgilliam <ggsmoustache@gmail.com>
2020-03-16 15:33:23 -0500

    workin on some seeds

---
+++ src/Actions/ChoosePlowedOrNatural.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using Trestlebridge.Interfaces;
+using Trestlebridge.Models;
+using Trestlebridge.Models.Plants;
+using Trestlebridge.Models.Facilities;
+
+namespace Trestlebridge.Actions
+{
+  public class ChoosePlowedOrNatural
+  {
+
+    // figure out a way for natural and plowed fields to exist in the same list 
+    // or
+    // run another for loop
+    public static void CollectInput(Farm farm, INatural plant)
+    {
+      Utils.Clear();
+
+      for (int i = 0; i < farm.NaturalFields.Count; i++)
+      {
+        Console.WriteLine($"{i + 1}. Natural Field");
+        Console.WriteLine($"\t This field has {farm.NaturalFields[i].PlantCount()} plants currently!\n");
+      }
+      for (int i = farm.NaturalFields.Count; i < farm.PlowedFields.Count + farm.NaturalFields.Count; i++)
+      {
+        Console.WriteLine($"{i + 1}. Plowed Field");
+        var plowedFieldIndex = i - farm.NaturalFields.Count;
+        Console.WriteLine($"\t This field has {farm.PlowedFields[plowedFieldIndex].PlantCount()} plants currently!\n");
+
+      }
+
+
+
+      Console.WriteLine();
+
+      // How can I output the type of plant chosen here?
+      Console.WriteLine($"Place the plant where?");
+
+      Console.Write("> ");
+      int choice = Int32.Parse(Console.ReadLine());
+
+      farm.NaturalFields[choice - 1].AddResource(plant);
+
+      /*
+          Couldn't get this to work. Can you?
+          Stretch goal. Only if the app is fully functional.
+       */
+      // farm.PurchaseResource<IPlowing>(plant, choice);
+
+    }
+  }
+}
--- src/Models/Plants/Sesame.cs
+++ src/Models/Plants/Sesame.cs
@@ -3,17 +3,19 @@ using Trestlebridge.Interfaces;
 
 namespace Trestlebridge.Models.Plants
 {
-    public class Sesame : IResource, ISeedProducing
+  public class Sesame : IResource, ISeedProducing, IPlowing
+  {
+    public int SeedsProduced { get; set; } = 520;
+    public string Type { get; } = "Sesame";
+
+    public double Harvest()
     {
-        private int _seedsProduced = 40;
-        public string Type { get; } = "Sesame";
-
-        public double Harvest () {
-            return _seedsProduced;
-        }
-
-        public override string ToString () {
-            return $"Sesame. Yum!";
-        }
+      return SeedsProduced;
     }
+
+    public override string ToString()
+    {
+      return $"Sesame. Yum!";
+    }
+  }
 }
