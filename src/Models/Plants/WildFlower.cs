8a2ab7f33895024517b0df861f28f16790bf4113
lcluischavez <lcluischavezz@gmail.com>
2020-03-16 10:47:21 -0500

    Flowers

--- src/Models/Plants/WildFlower.cs
+++ src/Models/Plants/WildFlower.cs
@@ -1,1 +1,19 @@
+using System;
+using Trestlebridge.Interfaces;
 
+namespace Trestlebridge.Models.Plants
+{
+    public class WildFlower : IResource, ISeedProducing
+    {
+        private int _seedsProduced = 40;
+        public string Type { get; } = "Wildflower";
+
+        public double Harvest () {
+            return _seedsProduced;
+        }
+
+        public override string ToString () {
+            return $"Wildflower. Cool!";
+        }
+    }
+}
