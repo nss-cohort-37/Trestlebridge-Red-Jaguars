5f0f2f6231e8fefeeba87ed196072e6dacbdd98f
tylerdav <tdavis1433@att.net>
2020-03-17 10:02:02 -0500

    grazing capacity

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -9,28 +9,28 @@ namespace Trestlebridge.Actions
 {
   public class ChooseGrazingField
   {
-    public static void CollectInput(Farm farm, IGrazing animal)
+    public static void CollectInput (Farm farm, IGrazing animal)
     {
-      Utils.Clear();
+      Utils.Clear ();
 
       for (int i = 0; i < farm.GrazingFields.Count; i++)
       {
-        Console.WriteLine($"{i + 1}. Grazing Field");
-        Console.WriteLine($"\t This field has {farm.GrazingFields[i].AnimalCount()} animals currently!\n");
+        if (farm.GrazingFields[i].AnimalCount () < 20)
+        {
+          Console.WriteLine ($"{i + 1}. Grazing Field");
+          Console.WriteLine ($"\t This field has {farm.GrazingFields[i].AnimalCount()} animals currently!\n");
+        }
       }
 
-      {
-      }
-
-      Console.WriteLine();
+      Console.WriteLine ();
 
       // How can I output the type of animal chosen here?
-      Console.WriteLine($"Place the animal where?");
+      Console.WriteLine ($"Place the animal where?");
 
-      Console.Write("> ");
-      int choice = Int32.Parse(Console.ReadLine());
+      Console.Write ("> ");
+      int choice = Int32.Parse (Console.ReadLine ());
 
-      farm.GrazingFields[choice - 1].AddResource(animal);
+      farm.GrazingFields[choice - 1].AddResource (animal);
 
       /*
           Couldn't get this to work. Can you?
